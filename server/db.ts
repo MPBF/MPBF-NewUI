80a656c08ae05ae4683721e661acd0197b70c5c4
AbuKhalid <modplast83@gmail.com>
2025-04-07 04:42:29 +0300

    Database

--- server/db.ts
+++ server/db.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from '../shared/schema';
